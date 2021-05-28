62eda86a8fbecc298b43b4fc885f36c6bb2f9e5b
la-morikawa <39390999+la-morikawa@users.noreply.github.com>
2021-05-28 16:10:28 +0900

    Add files via upload

---
+++ docs/a_book.sql
@@ -0,0 +1,22 @@
+﻿-- books table
+
+insert into books (books_name, books_genre) values ('精霊の守り人','ファンタジー'):
+insert into books (books_name, books_genre) values ('月の影　影の海','ファンタジー'):
+insert into books (books_name, books_genre) values ('空色勾玉','ファンタジー'):
+insert into books (books_name, books_genre) values ('獣の奏者　闘蛇編','ファンタジー'):
+insert into books (books_name, books_genre) values ('霧のむこうのふしぎな町','ファンタジー'):
+insert into books (books_name, books_genre) values ('銀河英雄伝説列伝1 晴れあがる銀河','SF'):
+insert into books (books_name, books_genre) values ('彼女は一人で歩くのか？ Does She Walk Alone？','SF'):
+insert into books (books_name, books_genre) values ('るん（笑）','SF'):
+insert into books (books_name, books_genre) values ('復活の日','SF'):
+insert into books (books_name, books_genre) values ('空の中','SF'):
+insert into books (books_name, books_genre) values ('ぼぎわんが、来る','ホラー'):
+insert into books (books_name, books_genre) values ('ホーンテッド・キャンパス','ホラー'):
+insert into books (books_name, books_genre) values ('ずうのめ人形','ホラー'):
+insert into books (books_name, books_genre) values ('ゴーストハント1 旧校舎怪談','ホラー'):
+insert into books (books_name, books_genre) values ('怪談喫茶ニライカナイ','ホラー'):
+insert into books (books_name, books_genre) values ('すべてがFになる','サスペンス'):
+insert into books (books_name, books_genre) values ('『アリス・ミラー城』殺人事件','サスペンス'):
+insert into books (books_name, books_genre) values ('屍人荘の殺人','サスペンス'):
+insert into books (books_name, books_genre) values ('ジョーカー・ゲーム','サスペンス'):
+insert into books (books_name, books_genre) values ('悪魔の手毬唄','サスペンス');
