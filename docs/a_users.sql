6a6f13b07ff528c1bd08c01c2b1ddf3ff5e42e46
la-morikawa <39390999+la-morikawa@users.noreply.github.com>
2021-05-31 10:45:02 +0900

    Add files via upload

---
+++ docs/a_users.sql
@@ -0,0 +1,12 @@
+﻿-- users テーブル insert用
+
+insert into users (user_id, password, user_name) values('00000001','pass01','Aoyama');
+insert into users (user_id, password, user_name) values('00000002','pass02','Kakita');
+insert into users (user_id, password, user_name) values('00000003','pass03','Saito');
+insert into users (user_id, password, user_name) values('00000004','pass04','Tanaka');
+insert into users (user_id, password, user_name) values('00000005','pass05','Narita');
+insert into users (user_id, password, user_name) values('00000006','pass06','Harui');
+insert into users (user_id, password, user_name) values('00000007','pass07','Makabe');
+insert into users (user_id, password, user_name) values('00000008','pass08','Yanagi');
+insert into users (user_id, password, user_name) values('00000009','pass09','Rakuyama');
+insert into users (user_id, password, user_name) values('00000010','pass10','Watanabe');
